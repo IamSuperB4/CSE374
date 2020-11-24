b1e964e866b5df6ac475dee849c61732b61b71ce
Jordan <jjh25@uw.edu>
2020-11-23 17:25:52 -0800

    Jordan Saved Code

---
+++ gitpull.bat
@@ -0,0 +1,2 @@
+cd C:\Users\hillt\Documents\CSE374\Homework3\CSE374
+git pull
---
+++ gitsavecode.bat
@@ -0,0 +1,4 @@
+cd C:\Users\hillt\Documents\CSE374\Homework3\CSE374
+git add .
+git commit -m "Jordan Saved Code"
+git push
